3f8cb8b9f30b2ced989eb98dcc5f19a4955b74ea
hecmung <kenwey3018@gmail.com>
2023-05-08 20:56:59 -0600

    Practica 08-05-2023 tercer filtro sin condiciones iniciales

---
+++ tercer_filtro.m
@@ -0,0 +1,19 @@
+%Mismo ejercicio que el archivo de 'segundo_filtro' con la excepción de que
+%no hay condiciones iniciales
+
+y = zeros(1,20) %Vector para guardar y
+y = [0 0 y] %Sin condiciones
+n = [0:1:19] %Vector con los 20 valores de n
+x = (0.8).^n %Senial x
+x = [0 0 x] %Sin condiciones 
+%Calculando resultados con un bucle for
+for n=3:20
+    y(n) = 2*x(n)-4*x(n-1)-0.5*y(n-1)-y(n-2)
+end
+
+n = [0:1:19] %Vector con los 20 valores de n
+subplot (3, 1, 3)
+stem (n(3:20), y(3:20))
+grid
+ylabel ('Salida')
+xlabel ('Muestra') %Salida de Y
